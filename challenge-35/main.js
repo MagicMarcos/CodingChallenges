46cbb8c0edccb11a5744123ed43d3469ae6b6e82
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-04-23 22:23:40 -0400

    conditional logic

---
+++ challenge-35/main.js
@@ -0,0 +1,11 @@
+// A function that returns based on string length 
+
+
+function names (n){
+    if (n > 5) {
+        return "wow thats a long name "
+    }
+    else {
+        return "what a nice short name "
+    }
+}
