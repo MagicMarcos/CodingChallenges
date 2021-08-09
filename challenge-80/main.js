cdf707ff1bb5b43279ed588207cf2cfb3e6d5403
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-08-09 14:23:09 -0400

    optimized challenge

--- challenge-80/main.js
+++ challenge-80/main.js
@@ -1,19 +1,35 @@
 // Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 2, print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s divisible by both 2 and 3, print out “Fizz Buzz”.
 
-function fizzbuzz(num) {
-	let numArr = [];
-	for (let i = 1; i <= num; i++) {
-		numArr.push(i);
-	}
-	return numArr.map(el =>
-		el % 3 === 0 && el % 2 === 0
-			? (el = 'fizzbuzz')
-			: el % 3 === 0
-			? (el = 'buzz')
-			: el % 2 === 0
-			? (el = 'fizz')
-			: el
-	);
-}
+// BRUTE FORCE
+// function fizzbuzz(num) {
+// 	let numArr = [];
+// 	for (let i = 1; i <= num; i++) {
+// 		numArr.push(i);
+// 	}
+// 	return numArr.map(el =>
+// 		el % 3 === 0 && el % 2 === 0
+// 			? (el = 'fizzbuzz')
+// 			: el % 3 === 0
+// 			? (el = 'buzz')
+// 			: el % 2 === 0
+// 			? (el = 'fizz')
+// 			: el
+// 	);
+// }
 
-console.log(fizzbuzz(10));
+//OPTIMAL
+const fizzBuzz = number => {
+	let output = [];
+	for (let i = 1; i <= number; i++) {
+		i % 6 === 0
+			? output.push('Fizz Buzz')
+			: i % 2 === 0
+			? output.push('Fizz')
+			: i % 3 === 0
+			? output.push('Buzz')
+			: output.push(i);
+	}
+	return output;
+};
+
+console.log(fizzBuzz(10));
