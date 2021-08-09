c229d53269599e99c9dc5d2e184a8680c372b7fb
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-08-09 14:12:51 -0400

    newChallenge

--- challenge-80/main.js
+++ challenge-80/main.js
@@ -1,3 +1,5 @@
+// Given a number as an input, print out every integer from 1 to that number. However, when the integer is divisible by 2, print out “Fizz”; when it’s divisible by 3, print out “Buzz”; when it’s divisible by both 2 and 3, print out “Fizz Buzz”.
+
 function fizzbuzz(num) {
 	let numArr = [];
 	for (let i = 1; i <= num; i++) {
