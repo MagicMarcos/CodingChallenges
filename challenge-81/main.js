5611a29c59f19b3b988053ec266e37700901b3e0
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-08-11 10:01:00 -0400

    quick update

--- challenge-81/main.js
+++ challenge-81/main.js
@@ -1,4 +1,6 @@
-//reverse a string of characters without using the reverse method
+//reverse a string of characters without using the reverse method. And what would you say about the split reverse join method.
+
+//In an interview take a up a little bit of extra time and say something like --- well I could use split reverse join but let's pretend we didnt have it. I think I could solve it with a simple for loop
 
 function reverseWord(str) {
 	let newStr = '';
