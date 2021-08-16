b6532648b77329bf1c368da77e663595eca746d1
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-08-16 14:53:44 -0400

    newC

---
+++ challenge-83/main.js
@@ -0,0 +1,5 @@
+// Given an array of items, reverse the order.
+
+// No reverse method! Unless that is your brute force first...
+
+// Hint: Loop through half of the array updating the array so that values at the front swap with values at the end
