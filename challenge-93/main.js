40598e1786de069f14bf5e526a83b5e05771c7a6
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-09-13 12:13:07 -0400

    newC

---
+++ challenge-93/main.js
@@ -0,0 +1,20 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+function check(a, x) {
+	let arrCheck = [];
+	a.forEach(el => {
+		if (el === x) {
+			arrCheck.push(x);
+		}
+	});
+
+	if (arrCheck.length > 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
