3dc9f9aacba143f87eb7e5db3909a8e3cf4746cd
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-10-07 18:01:11 -0400

    new C

---
+++ challenge-110/main.js
@@ -0,0 +1,8 @@
+// create a function that will return an array from 1 to n
+[...Array(10).keys()].map(x => x++);
+// create another function that returns an array from 0 to n-1
+[...Array(10).keys()];
+// create a function that adds all numbers from 1 to n
+function f(n) {
+	return (n * (n + 1)) / 2;
+}
