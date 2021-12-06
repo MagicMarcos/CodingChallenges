4643d06d213f6787ccc5670510cba7ddd7ac3bcb
Marcos Cardoso <76229820+MagicMarcos@users.noreply.github.com>
2021-12-05 21:46:50 -0500

    newC

---
+++ challenge-145/main.js
@@ -0,0 +1,6 @@
+// 'Create a Wu-Tang Clan name generator. Present the user with 5 survey questions and based on those answers randomly generate their name. ES6+, running on a node server, and should return a first and last name';
+const first = ['david', 'marcos', 'ernest', 'nathan'];
+const last = ['ney', 'cardoso', 'poh', 'quan'];
+
+// add up value of users choices that will be the array
+// max sum needs to be the length of the array
